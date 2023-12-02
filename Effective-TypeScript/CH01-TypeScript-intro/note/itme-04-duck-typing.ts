cab59ea08214987cb41f3b2fa463f198dc7e6139
thdud <thgml1444@naver.com>
2023-12-02 22:43:20 +0900

    TypeScript는 구조적 타이핑의 특징을 가지고 있어, 상위-하위 타입들의 호환이 가능하다.

---
+++ Effective-TypeScript/CH01-TypeScript-intro/note/itme-04-duck-typing.ts
@@ -0,0 +1,34 @@
+interface Vector2D {
+  x: number;
+  y: number;
+}
+interface NamedVector {
+  name: string;
+  x: number;
+  y: number;
+}
+
+function calculateLength(v: Vector2D) {
+  console.log(v);
+  return Math.sqrt(v.x * v.x + v.y * v.y);
+}
+
+const v:NamedVector = { x: 3, y: 4, name: "thdud" };
+console.log(calculateLength(v));
+/**
+  const v의 타입을 NamedVector로 설정, 
+  calculateLength의 매개변수의 타입은 Vector2D로 설정하였다. 
+  calculateLength함수에는 namedVector의 타입을 가진 v를 인수로 넣었는데,
+  함수의 실행에서 Error가 발생하지 않았다. 
+  NamedVector와 Vector2D의 관계를 정의하지도 않았는데 말이다.
+
+  section03, chapter01
+  NamedVector의 type과 Vector2의 type이 호환되기 때문에 
+  구조적 타이핑의 관점에서 보았을 때, 에러가 발생하지않은 것이다.  
+
+  <객체 타입의 호환성>
+ * 객체 타입을 다른 객체 타입으로 취급해도 괜찮은가? ㅇㅇ
+ * 객체는 더 적은 프로퍼티를 가지고 있는 type(Vector2D)이 상위type이 된다.
+ * 타입의 구조가 (부분적으로)일치하므로 구조적 타이핑의 관점으로 
+ * 두 타입은 호환되어 NamedVector타입이 Vector2D타입으로 취급됨.
+ */
---
+++ Effective-TypeScript/CH01-TypeScript-intro/note/itme-04-duck-typing2.ts
@@ -0,0 +1,38 @@
+interface Vector2D {
+  x: number;
+  y: number;
+}
+
+function calculateLength(v: Vector2D) {
+  console.log(v);
+  console.log(v.x * v.x + v.y * v.y);
+  return Math.sqrt(v.x * v.x + v.y * v.y);
+}
+
+interface Vector3D {
+  x: number;
+  y: number;
+  z: number;
+}
+
+function normalize(v: Vector3D) {
+  const length = calculateLength(v);
+  console.log("length-", length);
+  return {
+    x: v.x / length,
+    y: v.y / length,
+    z: v.z / length,
+  };
+}
+console.log(normalize({ x: 3, y: 4, z: 5 }));
+
+/**
+ normalize함수의 매개변수v의 타입은 Vector3이고, 
+ nor함수 안의 calculationLength함수가 호출이된다.
+ 호출된 cal함수의 매개변수의 타입은 Vector2다. 
+ 
+ Vector3과 Vector2의 매개변수의 개수는 다르지만 
+ 서로 타입이 같고, x와 y가 구조적으로 겹치기 때문에 에러가 발생하지 않고 호환되었다  
+ 하지만 이런 경우 에러를 체크해주는 편이 더 정확한 코드가 될 수 있다 
+ ( * item 27에서 추가로 에러 체크하는 법 나옴 )
+  */
