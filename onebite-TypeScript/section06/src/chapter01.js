5325ccb958fe60fc69dba4dd50f1a3f7a6dc2ff8
soyoung <thgml1444@naver.com>
2023-12-04 10:13:39 +0900

    Javascritp의 class를 통해 객체를 생성할 수 있다

---
+++ onebite-TypeScript/section06/src/chapter01.js
@@ -0,0 +1,73 @@
+/**
+ * JavaScript의 class문법
+ * 객체를 만들어낼 수 있다. 
+ */
+
+let studentA = {
+  name: "이정환",
+  grade: "A+",
+  age: 27,
+  study() {
+    console.log("열심히 공부 함");
+  },
+  introduce() {
+    console.log("안녕하세요!");
+  },
+};
+
+
+class Student {
+  // 필드
+  name;
+  grade;
+  age;
+
+  // 생성자 : 실제 객체를 만드는 메서드
+  constructor(name, grade ,age){
+    this.name = name;
+    this.grade = grade;
+    this.age = age;
+  }
+  
+  // 메서드
+  study(){
+    console.log('열심히 공부함')
+  }
+  introduce(){
+    console.log(`안녕하세요. ${this.name} 입니다`)
+  }
+}
+
+class Developer extends Student {
+  // 필드
+  language;
+
+  // 생성자
+  constructor (name, grade, age, language){
+    super(name, grade, age)
+    this.language = language;
+  }
+  
+  // 메서드
+  study(){
+    console.log('열심히 공부함')
+  }
+  introduce(){
+    console.log(`안녕하세요. ${this.name} 입니다`)
+  }
+  programming(){
+    console.log(`${this.language}로 개발함`)
+  }
+}
+
+let developer = new Developer('소영이', 'A+', 20, 'TypeScript')
+console.log(developer)
+developer.study()
+developer.introduce()
+developer.programming()
+
+// let studentB = new Student('정소영', 'A+' , 20)
+// console.log(studentB)
+// studentB.study()
+// studentB.introduce()
+
