159661be34c569087169f2bea53b3fc18e9568c0
soyoung <thgml1444@naver.com>
2023-12-09 11:02:07 +0900

    item 14 - 타입연산과 제너릭으로 반복 줄이기

--- Effective-TypeScript/CH02-Type-System/note/item14.ts
+++ Effective-TypeScript/CH02-Type-System/note/item14.ts
@@ -1,3 +1,6 @@
+/**
+ * 타입연산과 제너릭 사용으로 반복줄이기 
+ */
 const surfaceArea = (r,h) => 2* Math.PI *r * (r+h)
 const volume = (r,h) => Math.PI * r*r*h
 for (const [r,h] of [[1,1],[1,2],[2,1]]){
@@ -7,3 +10,38 @@ for (const [r,h] of [[1,1],[1,2],[2,1]]){
   Volune: ${volume(r, h)}
   `)
 }
+
+// 값의 형태에 해당하는 타입을 정의 (1) typeof 사용
+const INIT_OPTIONS = {
+  width: 640,
+  height: 480,
+  color: '#00FF00',
+  label: 'VGA',
+};
+// interface Options {
+//   width: number;
+//   height: number;
+//   color: string;
+//   label: string;
+// }
+type Options = typeof INIT_OPTIONS;
+
+
+
+// 반환값에 명명된 타입을 정의, 
+// ReturnType<T> : 타입변수 T에 할당된 함수 타입의 반환값을 추출하는 타입
+function getUserInfo(userId: string) {
+  // COMPRESS
+  const name = 'Bob';
+  const age = 12;
+  const height = 48;
+  const weight = 70;
+  const favoriteColor = 'blue';
+  // END
+  return { userId, name, age, height, weight, favoriteColor };
+}
+// Return type inferred as { userId: string; name: string; age: number, ... }
+
+type UserInfo = ReturnType<typeof getUserInfo>;
+
+
