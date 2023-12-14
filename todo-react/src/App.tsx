57d100461e76120a68c2ef0da8ce155209acf6b0
thdud <thgml1444@naver.com>
2023-12-14 16:35:56 +0900

    start Typescript + react todo app

---
+++ todo-react/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>우와 난 이제 공부뿐이야.</h1>
+    </div>
+  );
+}
+
+export default App;
