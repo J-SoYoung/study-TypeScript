76686534c0a7dd2e8101664145778fa93ce61600
thdud <thgml1444@naver.com>
2023-12-15 15:02:12 +0900

    create SelectBox components and interface selectbox and select item enum type

--- todo-react/src/components/createTaskForm/CreateTaskForm.tsx
+++ todo-react/src/components/createTaskForm/CreateTaskForm.tsx
@@ -3,6 +3,9 @@ import React, { FC, ReactElement } from 'react';
 import { TaskTitleField } from './_taskTitleField';
 import { TaskDeascriptionField } from './_taskDeascriptionField';
 import { TaskDateField } from './_taskDateField';
+import { TaskSelectField } from './_taskSelectField';
+import { Status } from './enums/Status';
+import { Priority } from './enums/Priority';
 
 export const CreateTaskForm: FC = (): ReactElement => {
   return (
@@ -21,6 +24,49 @@ export const CreateTaskForm: FC = (): ReactElement => {
         <TaskTitleField />
         <TaskDeascriptionField />
         <TaskDateField />
+        <Stack
+          sx={{ width: '100%' }}
+          direction="row"
+          spacing={2}
+        >
+          <TaskSelectField
+            label="Status"
+            name="status"
+            items={[
+              {
+                value: Status.todo,
+                label: Status.todo.toUpperCase(),
+              },
+              {
+                value: Status.inProgress,
+                label: Status.inProgress.toUpperCase(),
+              },
+              {
+                value: Status.completed,
+                label: Status.completed.toUpperCase(),
+              },
+            ]}
+          />
+          <TaskSelectField
+            label="Priority"
+            name="priority"
+            items={[
+              {
+                value: Priority.high,
+                label: Priority.high.toUpperCase(),
+              },
+              {
+                value: Priority.normal,
+                label: Priority.normal.toUpperCase(),
+              },
+              {
+                value: Priority.low,
+                label: Priority.low.toUpperCase(),
+              },
+            ]}
+          />
+          
+        </Stack>
       </Stack>
     </Box>
   );
