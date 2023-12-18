24331166dc3fc7abba3a89131e325e5a2d531abc
thdud <thgml1444@naver.com>
2023-12-18 14:12:47 +0900

    TaskArea는 두 개의 컴포넌트로 분리될 수 있다. - todoList의 상태에 따라 숫자를 셀 수 있는 TaskCount와 Todo의 내용을 적는 Task컴포넌트로 구분하여 구현하였다

--- todo-react/src/components/taskArea/TaskArea.tsx
+++ todo-react/src/components/taskArea/TaskArea.tsx
@@ -1,10 +1,56 @@
 import React, { FC, ReactElement } from 'react';
-import { Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
+import { format } from 'date-fns';
+import { TaskCounter } from '../tasckCounter/TaskCounter';
+import { Status } from '../createTaskForm/enums/Status';
+import { Task } from '../task/Task';
 
 export const TaskArea: FC = (): ReactElement => {
   return (
-    <Grid item md={8} px={4}>
-      <h2>content-- area</h2>
-    </Grid>
+    <Box width={'70%'}>
+      <Grid item md={8} px={4}>
+        <h2>
+          Status of your Tasks as on{' '}
+          {format(new Date(), 'PPPP')}
+        </h2>
+      </Grid>
+      <Grid
+        container
+        display={'flex'}
+        justifyContent={'center'}
+      >
+        <Grid
+          item
+          display={'flex'}
+          alignItems={'center'}
+          flexDirection={'row'}
+          justifyContent={'space-around'}
+          md={10}
+          xs={12}
+          mb={8}
+        >
+          <TaskCounter />
+          <TaskCounter />
+          <TaskCounter />
+        </Grid>
+        <Grid
+          item
+          display={'flex'}
+          flexDirection={'column'}
+          md={10}
+          xs={12}
+        >
+          <Grid>
+            <Task />
+          </Grid>
+          <Grid>
+            <Task />
+          </Grid>
+          <Grid>
+            <Task />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
