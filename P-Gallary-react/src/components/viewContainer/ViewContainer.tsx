0fa533f276202ff3faba080dff12a885511f9dc6
thdud <thgml1444@naver.com>
2023-12-22 12:21:45 +0900

    [gallary] inputContainer에서 입력받은 데이터를 viewContainer에 넘겨주어야 함으로 MainBoard에서 전체적인 state를 관리할 수 있도록 하였다. - image나 text와 같은 개별적인 데이터는 각 컴포넌트에서 관리하되, 그들을 모은 list는 main에서 관리하도록 onClick함수를 props으로 내려서 함수의 인자로 text,image를 받아올 수 있도록 하였다.

--- P-Gallary-react/src/components/viewContainer/ViewContainer.tsx
+++ P-Gallary-react/src/components/viewContainer/ViewContainer.tsx
@@ -1,5 +1,53 @@
 import React, { FC, ReactElement } from 'react';
+import styled from 'styled-components';
+import { Album } from '../inputContainer/types/typeAlbum';
 
-export const ViewContainer: FC = (): ReactElement => {
-  return <div>ViewContainer</div>;
+type Props = {
+  albumList: Album[];
 };
+
+export const ViewContainer: FC<Props> = (
+  props,
+): ReactElement => {
+  const { albumList } = props;
+
+  return (
+    <ViewBox>
+      {albumList.map((album, idx) => {
+        return (
+          <div key={idx} className="albumBox">
+            <img
+              src={album.image}
+              width={'170px'}
+              height={'180px'}
+            />
+            <p>{album.text}</p>
+          </div>
+        );
+      })}
+    </ViewBox>
+  );
+};
+
+const ViewBox = styled.div`
+  padding: 18px;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  .albumBox {
+    width: 180px;
+    margin: 16px 6px;
+    text-align: center;
+    p {
+      display: block;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      text-align: center;
+    }
+    img {
+      object-fit: cover;
+    }
+  }
+`;
--- P-Gallary-react/src/pages/MainBoard.tsx
+++ P-Gallary-react/src/pages/MainBoard.tsx
@@ -1,18 +1,31 @@
-import React, { FC, ReactElement } from 'react';
-
+import React, { FC, ReactElement, useState } from 'react';
 import { InputContainer } from '../components/inputContainer/InputContainer';
 import { ViewContainer } from '../components/viewContainer/ViewContainer';
+import { Album } from '../components/inputContainer/types/typeAlbum';
 import styled from 'styled-components';
 
 export const MainBoard: FC = (): ReactElement => {
+  // album-list
+  const [albumList, setAlbumList] = useState<Album[]>([]);
+
+  const onClickCreateAlbum = (
+    text: string,
+    image: string,
+  ) => {
+    setAlbumList([...albumList, { text, image }]);
+  };
+
   return (
     <MainContaier>
-        <Title>Gallary</Title>
-        <InputContainer />
-        <ViewContainer />
+      <Title>Gallary</Title>
+      <InputContainer
+        onClickCreateAlbum={onClickCreateAlbum}
+      />
+      <ViewContainer albumList={albumList} />
     </MainContaier>
   );
 };
+
 const MainContaier = styled.div`
   max-width: 1000px;
   margin: 0 auto;
