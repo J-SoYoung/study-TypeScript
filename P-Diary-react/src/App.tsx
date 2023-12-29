eb47c7f08750bcb3feeeed54193603cfe264b5bb
thdud <thgml1444@naver.com>
2023-12-30 08:40:52 +0900

    [ diayr ] App최상위 컴포넌트에서 useContext를 사용해 diarylist를 각 컴포넌트에서 바로 데이터를 사용할 수 있도록 하라. Home컴포넌트에서 diary를 만들고 view를 보여줄 때는 별 문제가 없었는데, detail에서 diaryList를 사용하려니까. 전역으로 상태를 관리해야 할 필요성이 생겼다. 그래서 useContextAPI를 사용하게 되었다. 지금은 list보여주는 부분의 context를 주입하였다. - context로 기본 diaryList와 setDiaryList를 생성하였고, 해당 타입도 지정 , 초기값도 설정하였다.

--- P-Diary-react/src/App.tsx
+++ P-Diary-react/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
 import './App.css';
 import { Home } from './pages/Home';
 import {
@@ -7,15 +7,39 @@ import {
   Routes,
 } from 'react-router-dom';
 import { DetailPage } from './pages/DetailPage';
+import { DiaryType } from './components/editor/types';
+
+export const DiaryStateContext = React.createContext<{
+  diaryList: DiaryType[];
+  setDiaryList: React.Dispatch<
+    React.SetStateAction<DiaryType[]>
+  >;
+}>({
+  diaryList: [],
+  setDiaryList: () => {},
+});
 
 const App: FC = (): ReactElement => {
+  const [diaryList, setDiaryList] = useState<DiaryType[]>(
+    [],
+  );
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail" element={<DetailPage />} />
-      </Routes>
-    </BrowserRouter>
+    <DiaryStateContext.Provider
+      value={{ diaryList, setDiaryList }}
+    >
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/detail/:id"
+              element={<DetailPage diaryList={diaryList} />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </DiaryStateContext.Provider>
   );
 };
 
