e5b9735f2f02985ee290d4a6b2074c7c8092d0b5
thdud <thgml1444@naver.com>
2023-12-29 15:02:23 +0900

    [ diary ] home컴포넌트에서는 diarylist의 상태를 관리하고,이들을 editor / DiaryList컴포넌트로 넘겨준다.

--- P-Diary-react/src/pages/Home.tsx
+++ P-Diary-react/src/pages/Home.tsx
@@ -1,12 +1,27 @@
-import React, { FC, ReactElement } from 'react';
-import { DiaryList } from '../components/diaryList/DiaryList';
+import React, { FC, ReactElement, useState } from 'react';
 import { Editor } from '../components/editor/Editor';
+import { DiaryType } from '../components/editor/types';
+import { DiaryList } from '../components/diaryList/DiaryList';
+import styled from 'styled-components';
 
 export const Home: FC = (): ReactElement => {
+  const [diaryList, setDiaryList] = useState<DiaryType[]>(
+    [],
+  );
+
   return (
-    <>
-      <Editor />
-      <DiaryList />
-    </>
+    <HomeContainer>
+      <Editor
+        diaryList={diaryList}
+        setDiaryList={setDiaryList}
+      />
+      <DiaryList diaryList={diaryList} />
+    </HomeContainer>
   );
 };
+
+const HomeContainer = styled.div`
+  max-width: 450px;
+  padding: 16px;
+  box-sizing: border-box;
+`;
