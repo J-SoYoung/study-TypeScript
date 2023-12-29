bcb36aebbe0218cfec9273405e66e62a611d4135
thdud <thgml1444@naver.com>
2023-12-30 08:47:18 +0900

    [ diary ] App에서 데이터를 props으로 공급받는 detailpage를 생성하였다. 추후 ContextAPI데이터로 바꿀 예정

--- P-Diary-react/src/pages/DetailPage.tsx
+++ P-Diary-react/src/pages/DetailPage.tsx
@@ -1,5 +1,61 @@
 import React, { FC, ReactElement } from 'react';
+import {
+  ButtonDiv,
+  DetailBox,
+  ImageDiv,
+  TextDiv,
+} from './style';
+import { useNavigate, useParams } from 'react-router-dom';
+import { DiaryType } from '../components/editor/types';
 
-export const DetailPage: FC = (): ReactElement => {
-  return <h1>DetailPage</h1>;
+type Props = {
+  diaryList: DiaryType[];
+};
+
+export const DetailPage: FC<Props> = (
+  props,
+): ReactElement => {
+  const { diaryList } = props;
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const diaryID = Number(id);
+  const diary = id ? diaryList[diaryID] : null;
+
+  if (diary === null) {
+    return (
+      <>
+        <h1> 다이어리가 없습니다. </h1>
+        <button
+          className="go_main_button"
+          onClick={() => navigate('/')}
+        >
+          메인으로
+        </button>
+      </>
+    );
+  }
+  return (
+    <DetailBox>
+      <ImageDiv $emotionColor={diary?.emotion.color}>
+        <img src={`/assets/${diary.emotion.src}`} />
+      </ImageDiv>
+      <TextDiv $emotionColor={diary?.emotion.color}>
+        <p className="date_p">
+          {diary.date.toISOString().split('T')[0]}
+        </p>
+        <p className="title_p">{diary.title}</p>
+        <p className="contents_p">{diary.contents}</p>
+      </TextDiv>
+      <ButtonDiv $emotionColor={diary?.emotion.color}>
+        <button
+          className="go_main_button"
+          onClick={() => navigate('/')}
+        >
+          메인으로
+        </button>
+        <button className="edit_button">수정하기</button>
+      </ButtonDiv>
+    </DetailBox>
+  );
 };
